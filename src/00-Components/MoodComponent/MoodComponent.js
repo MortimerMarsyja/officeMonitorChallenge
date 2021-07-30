17144d45184efd380f256013f497bb324ae9f265
marcos <marcoscolladosegura@ied.edu>
2021-07-30 14:49:12 +0200

    center icon

--- src/00-Components/MoodComponent/MoodComponent.js
+++ src/00-Components/MoodComponent/MoodComponent.js
@@ -8,17 +8,17 @@ const MoodComponent = ({ wellbeing }) => {
     console.log(moodArr)
     const handleShowIcon = () => {
         const casualties = moodArr.filter(item => item === true).length;
-        if (casualties === 0) return <p>&#9788;</p>
-        if (casualties === 1) return <p>&#9730;</p>
-        if (casualties === 2) return <p>&#9748;</p>
-        if (casualties === 3) return <p>&#9889;</p>
+        if (casualties === 0) return <p style={{ width: '100%' }}>&#9788;</p>
+        if (casualties === 1) return <p style={{ width: '100%' }}> &#9730;</p>
+        if (casualties === 2) return <p style={{ width: '100%' }}>&#9748;</p>
+        if (casualties === 3) return <p style={{ width: '100%' }}>&#9889;</p>
     }
 
     return (<div className="widget" id="mood">
         <div className="title">
             Mood
         </div>
-        <div className="content" sytle={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
+        <div className="content" >
             {handleShowIcon()}
         </div>
     </div >)
